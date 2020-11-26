29f0a02b8fa67cfe30dfb632d5a08664ab2e771b
lh77x7 <75083883+lh77x7@users.noreply.github.com>
2020-11-26 21:54:54 +0100

    Add files via upload

---
+++ drunk_robot.py
@@ -0,0 +1,45 @@
+# robot try to walk straight
+# right direction for the robot is walk from left to right straight
+
+import turtle
+
+turtle.speed(1)
+turtle.right(100)
+turtle.left(200)
+turtle.forward(200)
+
+turtle.right(100)
+turtle.left(200)
+turtle.forward(100)
+
+turtle.right(200)
+turtle.forward(200)
+
+turtle.right(114)
+turtle.forward(190)
+
+turtle.left(200)
+turtle.forward(200)
+
+turtle.left(200)
+turtle.forward(150)
+
+turtle.right(200)
+turtle.forward(50)
+
+turtle.left(200)
+turtle.forward(50)
+
+turtle.left(100)
+turtle.forward(200)
+
+turtle.right(200)
+turtle.forward(100)
+
+turtle.left(100)
+turtle.forward(100)
+
+turtle.left(75)
+turtle.forward(100)
+
+#finally robot find the way ...
