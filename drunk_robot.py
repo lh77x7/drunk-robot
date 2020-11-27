cbd314119e58de3041516fe2b01fd253e8bf7fdb
unknown <>
2020-11-27 17:24:59 +0100

    Create drunk_robot.py

---
+++ drunk_robot.py
@@ -0,0 +1,58 @@
+# robot try to walk straight
+# right direction for the robot is walk from left to right straight
+# move forward - turtle.forward(angle)
+# move left - turle.left (angle)
+# move right - turle.right (angle)
+
+
+import turtle
+import time
+
+turtle.right(100)
+turtle.left(200)
+turtle.forward(200)
+# wait 2 seconds
+time.sleep(1)
+
+turtle.right(100)
+turtle.left(200)
+turtle.forward(100)
+time.sleep(1)
+
+turtle.right(200)
+turtle.forward(200)
+time.sleep(1)
+
+turtle.right(114)
+turtle.forward(190)
+time.sleep(1)
+
+turtle.left(200)
+turtle.forward(200)
+time.sleep(1)
+
+turtle.left(200)
+turtle.forward(150)
+time.sleep(1)
+
+turtle.right(200)
+turtle.forward(50)
+
+turtle.left(200)
+turtle.forward(50)
+
+turtle.left(100)
+turtle.forward(100)
+
+turtle.right(200)
+turtle.forward(100)
+
+turtle.left(100)
+turtle.forward(100)
+time.sleep(1)
+
+turtle.left(75)
+turtle.forward(100)
+time.sleep(2)
+
+#finally robot find the way ...
